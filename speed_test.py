63bcfeaae2e9d09e555b2b01d480674f01cc99e3
wrs225 <wrs225@cornell.edu>
2024-06-09 22:00:58 -0700

    speed test

--- speed_test.py
+++ speed_test.py
@@ -9,14 +9,22 @@ plotting = True
 ana = Analysis("./neuron_mv")
 ana.set_target("fpga")
 # ana.build()  # build bitstream
-#ctrl = ana.launch(debug=True)  # program FPGA
-
-#v_out = ctrl.probe("v_out_probe")
+ctrl = ana.launch(debug=True)  # program FPGA
+ctrl.stall_emu()
+ctrl.set_reset(1)
+ctrl.set_reset(0)
+v_out = ctrl.probe("v_out_probe")
 y = []
 #time this 
+start = time.time()
 
-ana.emulate()
+while ctrl.get_emu_time() < 50e-3:
+    
+    ctrl.sleep_emu(50e-3)
+    y.append(v_out)
 
+end = time.time()
+print("Time elapsed: ", end-start)
 
 # Prepare the plot
 plt.plot(y)
